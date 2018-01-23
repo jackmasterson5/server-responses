f07cd2865acec7911c61e51bfbfce5270f1a3918
jackmasterson5 <jackmasterson.ext@gmail.com>
2018-01-23 17:38:56 -0500

    solve puzzle attempt 1

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.listen(PORT);
 console.log('app is listening on port: ', PORT);
 
 app.get('*', (req, res) => {
-    let analysis = Question.analysis();
+    let analysis = Question.analysis(req.url);
     new Question(req.url, analysis, res);
 });
 
 class Question {
-    static analysis() {
+    static analysis(url) {
         return {
             'Ping': 'OK',
             'Resume': 'https://docs.google.com/document/d/1SfoG7CdE0VYFax5zC1__NQF36ZAYEO78WDi9F6MBxiM/edit?usp=sharing',
@@ -27,12 +27,51 @@ class Question {
             'Phone': '908-433-0178',
             'Email+Address': 'jackmasterson5@gmail.com',
             'Position': 'Front End/Ad Tech Software Engineer',
-            'Puzzle': ' ABCD\n' +
-                        'A=>>>\n' +
-                        'B<=><\n' +
-                        'C<<=<\n' +
-                        'D<>>=',
+            'Puzzle': this.solvePuzzle(url),
         }
+    }
+
+    static solvePuzzle(url) {
+        let final = 'ABCD';
+        let stage = decodeURI(url.split('ABCD')[1]).split('\n');
+        let use = {
+            'A': stage[1],
+            'B': stage[2],
+            'C': stage[3],
+            'D': stage[4],
+        };
+
+        if (use['A'] !== undefined) {
+            let keys = Object.keys(use);
+            keys.map((a, b) => {
+                let row = use[a].slice(1);
+                row = row.replace('%3D', '=');
+                row = row.split('');
+                row[b] = '=';
+                row = row.join('');
+                row = this.replaceDashes(row);
+                final += ('\n' + a + row);
+            });
+            console.log('final: ', final);
+        }
+
+    }
+
+    static replaceDashes(r) {
+        let stage = [];
+        r = r.split('');
+        r.map((a,b) => {
+            if (a === '-') {
+                if (b % 2 === 0) {
+                    a = '>';
+                } else {
+                    a = '<';
+                }
+            }
+            stage.push(a);
+        });
+        return stage.join('');
+
     }
 
     constructor(url, analysis, res) {
@@ -46,11 +85,13 @@ class Question {
 
         this.analyze();
     }
+
     analyze() {
         let answer = this.analysis[this.question];
         
         this.sendData(this.res, answer);
     }
+
     sendData(res, answer) {
         res.send(answer);
     }
