306dfd9ae5766f04b97a85cd8430235f7b316e7b
jackmasterson5 <jackmasterson.ext@gmail.com>
2018-01-23 16:19:44 -0500

    fix linting errors

--- server.js
+++ server.js
@@ -9,22 +9,25 @@ app.listen(PORT);
 console.log('app is listening on port: ', PORT);
 
 app.get('*', (req, res) => {
+    Question.analysis = 
     new Question(req.url, res);
 });
 
 class Question {
-    static analysis = {
-        'Ping': 'OK',
-        'Resume': 'https://docs.google.com/document/d/1SfoG7CdE0VYFax5zC1__NQF36ZAYEO78WDi9F6MBxiM/edit?usp=sharing',
-        'Degree': 'Front End Developer "Nanodegree" from Udacity: https://www.udacity.com/',
-        'Name': 'Jack Masterson',
-        'Source': 'Private GitHub: https://github.com/jackmasterson5/server-responses',
-        'Years': '2-3 years of experience',
-        'Status': 'Yes, I can prove my eligibility',
-        'Referrer': 'I was contacted through Angel List by Jenny Gasparis',
-        'Phone': '908-433-0178',
-        'Email+Address': 'jackmasterson5@gmail.com',
-        'Position': 'Front End/Ad Tech Software Engineer',
+    static analysis() {
+        return {
+            'Ping': 'OK',
+            'Resume': 'https://docs.google.com/document/d/1SfoG7CdE0VYFax5zC1__NQF36ZAYEO78WDi9F6MBxiM/edit?usp=sharing',
+            'Degree': 'Front End Developer "Nanodegree" from Udacity: https://www.udacity.com/',
+            'Name': 'Jack Masterson',
+            'Source': 'Private GitHub: https://github.com/jackmasterson5/server-responses',
+            'Years': '2-3 years of experience',
+            'Status': 'Yes, I can prove my eligibility',
+            'Referrer': 'I was contacted through Angel List by Jenny Gasparis',
+            'Phone': '908-433-0178',
+            'Email+Address': 'jackmasterson5@gmail.com',
+            'Position': 'Front End/Ad Tech Software Engineer',
+        }
     }
 
     constructor(url, res) {
@@ -38,7 +41,8 @@ class Question {
         this.analyze();
     }
     analyze() {
-        let answer = this.analysis[this.q];
+        let analysis = Question.analysis();
+        let answer = analysis[this.q];
         this.sendData(this.res, answer);
     }
     sendData(res, answer) {
