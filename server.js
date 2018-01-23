b8af8244e9631f6767a5908074a1941f9756a60f
jackmasterson5 <jackmasterson.ext@gmail.com>
2018-01-23 16:15:13 -0500

    class based approach

--- server.js
+++ server.js
@@ -9,49 +9,39 @@ app.listen(PORT);
 console.log('app is listening on port: ', PORT);
 
 app.get('*', (req, res) => {
-
-    const sendData = (answer) => {
-        res.send(answer);
-    }
-
-    const formatUrl = () => {
-        let d = req.url.split('&d=')[0];
-        let q = d.split('/?q=')[1];
-        let data = util.analyzeQuestion(q, d, sendData);
-    }
-
-    formatUrl();
+    new Question(req.url, res);
 });
 
-const util = {
-    analyzeQuestion: (question, description, callback) => {
-        let answer;
-        console.log('question: ', question);
-        if (question === 'Ping') {
-            answer = 'OK';
-        } else if (question === 'Resume') {
-            answer = 'https://docs.google.com/document/d/1SfoG7CdE0VYFax5zC1__NQF36ZAYEO78WDi9F6MBxiM/edit?usp=sharing';
-        } else if (question === 'Degree') {
-            answer = 'Front End Developer "Nanodegree" from Udacity: https://www.udacity.com/';
-        } else if (question === 'Name') {
-            answer = 'Jack Masterson';
-        } /*else if (question = 'Source') {
-            answer = 'Private GitHub: https://github.com/jackmasterson5/server-responses';
-        }*/ else if (question = 'Years') {
-            answer = '2-3 years of experience';
-        } else if (question = 'Status') {
-            answer = 'Yes, I can prove my eligibility.';
-        } else if (question === 'Referrer') {
-            answer = 'I was contacted through Angel List by Jenny Gasparis.';
-        } else if (question = 'Phone') {
-            answer = '908-433-0178';
-        } else if (question = 'Email+Address') {
-            answer = 'jackmasterson5@gmail.com';
-        } else if (question = 'Position') {
-            answer = 'Front End/Ad Tech Software Engineer';
-        } else {
-            answer = 'Response did not match a known input';
-        }
-        callback(answer);
+class Question {
+    static analysis = {
+        'Ping': 'OK',
+        'Resume': 'https://docs.google.com/document/d/1SfoG7CdE0VYFax5zC1__NQF36ZAYEO78WDi9F6MBxiM/edit?usp=sharing',
+        'Degree': 'Front End Developer "Nanodegree" from Udacity: https://www.udacity.com/',
+        'Name': 'Jack Masterson',
+        'Source': 'Private GitHub: https://github.com/jackmasterson5/server-responses',
+        'Years': '2-3 years of experience',
+        'Status': 'Yes, I can prove my eligibility',
+        'Referrer': 'I was contacted through Angel List by Jenny Gasparis',
+        'Phone': '908-433-0178',
+        'Email+Address': 'jackmasterson5@gmail.com',
+        'Position': 'Front End/Ad Tech Software Engineer',
+    }
+
+    constructor(url, res) {
+        this.res = res;
+
+        let d = url.split('&d=')[0];
+        let q = d.split('/?q=')[1];
+
+        this.question = q;
+
+        this.analyze();
+    }
+    analyze() {
+        let answer = this.analysis[this.q];
+        this.sendData(this.res, answer);
+    }
+    sendData(res, answer) {
+        res.send(answer);
     }
 }
